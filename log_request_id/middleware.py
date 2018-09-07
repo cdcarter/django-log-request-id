c1b9f95644147d224e4c92b5833c882ff2c2e70d
Christian Carter <cdcarter@gmail.com>
2018-09-07 07:32:05 -0700

    format and change

--- log_request_id/middleware.py
+++ log_request_id/middleware.py
@@ -2,57 +2,66 @@ import logging
 import uuid
 
 from django.conf import settings
+
 try:
     from django.utils.deprecation import MiddlewareMixin
 except ImportError:
     MiddlewareMixin = object
-from log_request_id import local, REQUEST_ID_HEADER_SETTING, LOG_REQUESTS_SETTING, NO_REQUEST_ID, \
-    REQUEST_ID_RESPONSE_HEADER_SETTING, GENERATE_REQUEST_ID_IF_NOT_IN_HEADER_SETTING
+from log_request_id import (
+    local,
+    REQUEST_ID_HEADER_SETTING,
+    LOG_REQUESTS_SETTING,
+    NO_REQUEST_ID,
+    REQUEST_ID_RESPONSE_HEADER_SETTING,
+    GENERATE_REQUEST_ID_IF_NOT_IN_HEADER_SETTING,
+)
 
 
 logger = logging.getLogger(__name__)
 
 
 class RequestIDMiddleware(MiddlewareMixin):
-
     def process_request(self, request):
         request_id = self._get_request_id(request)
         local.request_id = request_id
         request.id = request_id
+        # try:
+        #    del local.request_id
+        # except AttributeError:
+        #    pass
 
     def process_response(self, request, response):
-        if getattr(settings, REQUEST_ID_RESPONSE_HEADER_SETTING, False) and getattr(request, 'id', None):
+        if getattr(settings, REQUEST_ID_RESPONSE_HEADER_SETTING, False) and getattr(
+            request, "id", None
+        ):
             response[getattr(settings, REQUEST_ID_RESPONSE_HEADER_SETTING)] = request.id
 
         if not getattr(settings, LOG_REQUESTS_SETTING, False):
             return response
 
         # Don't log favicon
-        if 'favicon' in request.path:
+        if "favicon" in request.path:
             return response
 
-        user = getattr(request, 'user', None)
-        user_id = getattr(user, 'pk', None) or getattr(user, 'id', None)
+        user = getattr(request, "user", None)
+        user_id = getattr(user, "pk", None) or getattr(user, "id", None)
 
-        message = 'method=%s path=%s status=%s'
+        message = "method=%s path=%s status=%s"
         args = (request.method, request.path, response.status_code)
 
         if user_id:
-            message += ' user=%s'
+            message += " user=%s"
             args += (user_id,)
 
         logger.info(message, *args)
-        
-        try:
-            del local.request_id
-        except AttributeError:
-            pass
 
         return response
 
     def _get_request_id(self, request):
         request_id_header = getattr(settings, REQUEST_ID_HEADER_SETTING, None)
-        generate_request_if_not_in_header = getattr(settings, GENERATE_REQUEST_ID_IF_NOT_IN_HEADER_SETTING, False)
+        generate_request_if_not_in_header = getattr(
+            settings, GENERATE_REQUEST_ID_IF_NOT_IN_HEADER_SETTING, False
+        )
 
         if request_id_header:
             # fallback to NO_REQUEST_ID if settings asked to use the
